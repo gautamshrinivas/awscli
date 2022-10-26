7b97bc8ca1c50b6340ce924599d59c76fcc85bf3
gautam <gautamrck76@gmail.com>
2022-10-26 14:43:59 +0530

    added sh

--- ec2.sh
+++ ec2.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 aws ec2 run-instances 
-  --image-id ami-08c40ec9ead489470 
-  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=activity}]'
-  --count 1 
-  --instance-type t2.micro 
-  --subnet-id subnet-0008be71a94c13266 
-  --security-group-ids sg-0e5e1e9926680a31b 
-  --associate-public-ip-address 
-  --key-name gautam-key
+  --image-id ami-08c40ec9ead489470 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=activity}]' --count 1 --instance-type t2.micro --subnet-id subnet-0008be71a94c13266 --security-group-ids sg-0e5e1e9926680a31b --associate-public-ip-address --key-name gautam-key
